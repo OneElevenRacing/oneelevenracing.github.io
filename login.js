54db15c926ae44e7a04bbdc58b3e9f90ab6590c8
OneElevenRacing <132026013+OneElevenRacing@users.noreply.github.com>
2024-04-04 15:42:59 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,30 +1,16 @@
 // Initialize Firebase as shown in previous examples
 
-// Function to check if the user is in a webview
-function isInWebview() {
-    // Check if the user agent contains keywords indicating a webview
-    var userAgent = navigator.userAgent.toLowerCase();
-    return userAgent.includes('wv') || userAgent.includes('webview');
-}
-
 // Sign-in function
 document.getElementById('signin-button').addEventListener('click', function() {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
         // Successful sign-in
-        if (isInWebview()) {
-            // Open the main page in a new browser window
-            window.open('main.html', '_blank');
-        } else {
-            // Redirect to main page
-            window.location.href = 'main.html';
-        }
+        window.location.href = 'main.html'; // Redirect to main page
     }).catch(function(error) {
         // Handle sign-in errors
         console.log('Sign-in error:', error);
     });
 });
-
 
 function logoutUser() {
     firebase.auth().signOut().then(function() {
